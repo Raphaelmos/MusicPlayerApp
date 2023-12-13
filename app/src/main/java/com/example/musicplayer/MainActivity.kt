350ffa9531ed605cffce4e95318c09656bdba7ca
Raphaelmos <96924364+Raphaelmos@users.noreply.github.com>
2023-12-13 05:32:54 +0100

    Update MainActivity.kt
    ajout commentaire bouton et mediaPlayer pour music

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -9,3 +9,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+/*
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+
+
+
+		val mediaplayer : MediaPlayer! = MediaPlayer.create( context: this,R.raw.music 
+
+		play_btn.setOnClickListener { it:View!
+
+			if(!mediaplayer.isPlaying){
+			    mediaplayer.start()
+			    play_btn.setImageResource(R.drawable.ic_pause_black_24dp)
+			}else{
+			mediaplayer.pause()
+			play_btn.setImageResource(R.drawable.ic_pause_black_24dp)
+			}
+		}
+	}
+}
+*/
+
+/*
